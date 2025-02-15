a5ff04f63ed7b102f37f110fa8d2f1391a90a62a
MohammadHomsee <91120152+MohammadHomsee@users.noreply.github.com>
2025-02-15 20:43:34 +0400

    commit

---
+++ server.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+app = FastAPI()
+
+clients:list[WebSocket] = []
+
+@app.websocket("/broadcast")
+async def hello_endpoint(websocket:WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    print('client connected')
+
+    try:
+        while True:
+            message:str = await websocket.receive_text()
+            target_clients = [client for client in clients if client is not websocket]
+            for client in target_clients:
+                await client.send_text(message)
+    except WebSocketDisconnect:
+        clients.remove(websocket)
+        print('client disconnected.')
