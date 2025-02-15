5612c1ebe79b69ec769996c0a7c6298535444138
MohammadHomsee <91120152+MohammadHomsee@users.noreply.github.com>
2025-02-15 20:47:32 +0400

    commit

---
+++ run.sh
@@ -0,0 +1,1 @@
+uvicorn server:app --host 0.0.0.0 --port 8000 --ssl-keyfile /etc/letsencrypt/live/nissan-kicks.click/privkey.pem --ssl-certfile /etc/letsencrypt/live/nissan-kicks.click/fullchain.pem
