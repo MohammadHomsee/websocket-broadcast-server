d853c13494050639376d4b9c401cdfd9d3e7060b
MohammadHomsee <91120152+MohammadHomsee@users.noreply.github.com>
2025-02-21 02:26:53 +0400

    commit

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-uvicorn server:app --host 0.0.0.0 --port 8000 --ssl-keyfile /etc/letsencrypt/live/thh-fire.site/privkey.pem --ssl-certfile /etc/letsencrypt/live/thh-fire.site/fullchain.pem
+./env/bin/python -m uvicorn server:app --host 0.0.0.0 --port 8000 --ssl-keyfile /etc/letsencrypt/live/thh-fire.site/privkey.pem --ssl-certfile /etc/letsencrypt/live/thh-fire.site/fullchain.pem
